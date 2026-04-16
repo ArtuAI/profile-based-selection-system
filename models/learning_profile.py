1c91809a429548159cdbed5bd8391f8a3c774450
ArtuAI <arturas.artiomovas@stud.vilniustech.lt>
2026-04-16 13:35:40 +0000

    learningProfiles class and methods

---
+++ models/learning_profile.py
@@ -0,0 +1,23 @@
+class LearningProfile:
+    def __init__(self, target_role, budget_level, weekly_hours, current_skills):
+        self._target_role = target_role
+        self._budget_level = budget_level
+        self._weekly_hours = weekly_hours
+        self._current_skills = current_skills
+
+    def display_info(self):
+        print("Target role:", self._target_role)
+        print("Budget level:", self._budget_level)
+        print("Weekly hours:", self._weekly_hours)
+        print("Current skills:", self._current_skills)
+
+    def add_skill(self, skill):
+        self._current_skills.append(skill)
+        
+    
+    def has_skill(self, skill_name):
+        for skill in self._current_skills:
+            if skill._name == skill_name:
+                return True
+        return False
+            
