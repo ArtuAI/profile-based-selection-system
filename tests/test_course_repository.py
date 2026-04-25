7703ef64295c8bb65004eb8ca63ecccf7b86f800
ArtuAI <arturas.artiomovas@stud.vilniustech.lt>
2026-04-25 15:35:04 +0000

    tests

--- tests/test_course_repository.py
+++ tests/test_course_repository.py
@@ -1,22 +1,13 @@
 import unittest
 from repository.course_repository import CourseRepository
-from models.course import Course
 
 
 class TestCourseRepository(unittest.TestCase):
-    def test_load_courses_returns_non_empty_list(self):
+    def test_load_courses(self):
         repo = CourseRepository("data/items.json")
         courses = repo.load_courses()
 
         self.assertGreater(len(courses), 0)
-
-    def test_load_courses_returns_course_objects(self):
-        repo = CourseRepository("data/items.json")
-        courses = repo.load_courses()
-
-        first_course = courses[0]
-
-        self.assertIsInstance(first_course, Course)
 
 
 if __name__ == "__main__":
